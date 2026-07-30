f340f5a261f64ea1a626aa5537ea3d5d95d4aca2
RuleGaed <arasomeroguz9@gmail.com>
2026-07-30 23:50:33 +0300

    switch to @astrojs/vercel@10.0.8 for Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,12 @@
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel";
 import svelte from "@astrojs/svelte";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
   integrations: [svelte()],
-  server: {
-    port: 3000,
-    host: true,
-  },
   vite: {
     plugins: [tailwindcss()],
     resolve: {
