9fae8b4f8ea2accec976902f782bb8d23b690565
RuleGaed <arasomeroguz9@gmail.com>
2026-07-31 10:45:20 +0300

    fix license link branch master to main

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -7,7 +7,7 @@
     { href: "/downloads", labelKey: "footer.links.downloads" },
     { href: siteConfig.links.github.repo, labelKey: "footer.links.githubRepo" },
     { href: `${siteConfig.links.github.repo}/issues`, labelKey: "footer.links.githubIssues" },
-    { href: `${siteConfig.links.github.repo}/blob/master/LICENSE.md`, labelKey: "footer.links.license" },
+    { href: `${siteConfig.links.github.repo}/blob/main/LICENSE.md`, labelKey: "footer.links.license" },
   ];
 
   const currentYear = new Date().getFullYear();
